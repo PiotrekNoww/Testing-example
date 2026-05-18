84c798d28a6b59aeba5648d79ba8b5629e01c292
majkowaty <polskinexuspl@gmail.com>
2026-05-18 19:31:28 +0200

    added contact functionality to student

---
+++ src/test/java/com/learning/courses/controller/PersonControllerIT.java
@@ -0,0 +1,32 @@
+package com.learning.courses.controller;
+
+import com.learning.courses.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+public class PersonControllerIT extends AbstractIntegrationTest {
+
+    @Test
+    void addContactToStudent(){
+
+    }
+
+    @Test
+    void addContactToStudentWithPersonThatIsNotAStudentShouldThrow(){
+
+    }
+
+    @Test
+    void addContactToStudentInvalidRequestShouldThrow(){
+
+    }
+
+    @Test
+    void removeContactFromStudent(){
+
+    }
+
+    @Test
+    void removeContactFromStudentInvalidPersonIdShouldThrow(){
+
+    }
+}
